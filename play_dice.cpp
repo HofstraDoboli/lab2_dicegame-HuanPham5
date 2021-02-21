c2acf10b90074d634d4f2101245ea0995fd63276
HuanPham5 <HuanPham5@pride.hofstra.edu>
2021-02-21 21:55:03 +0000

    answer to dice game v1

--- play_dice.cpp
+++ play_dice.cpp
@@ -14,7 +14,8 @@ int main()
 
     // Add your code to play the dice game here
     bool play = false;
-    string response,response1,response2,response3;
+    bool play2 = false;
+    string response,response1,response2,response3,response4,response5;
     cout<<"Welcome to craps dice game!"<<endl;
     cout<<"Rules: If the sum of your dice is 7 or 11, you win!"<<endl;
     cout<<"But if the sum of your dice is 2 or 3 or 12, you lose..."<<endl;
@@ -24,12 +25,12 @@ int main()
     {
         play = true;
     
-    //for (int i = 0; i < 50; i ++)
         while (play)
         {
-            int dice_output1 = rand()%6+1; //change this expression so dice_output has a value between 1 and 6
+            int dice_output1 = rand()%6+1;
             int dice_output2 = rand()%6+1  ; 
             int sum;
+            
             sum = dice_output1 + dice_output2;
             cout <<"You rolled: "<<dice_output1 <<" and "<<dice_output2<<endl;
             cout <<"So your sum is: "<<sum<<endl;
@@ -42,25 +43,66 @@ int main()
                   continue;
                else
                  play = false;
+                 cout<<"Thanks for playing!"<<endl;
                
             }
             else if (sum ==2 || sum==3 || sum==12)
             { 
                cout<<"Sorry you lost..."<<endl;
                cout<<"Try again? (y or no)"<<endl;
+               cin>>response2;
                if (response2=="y")
-                  continue;
-               else
-                  break;
-            }
-            else
-               cout<<"Not winning nor losing, roll again?(y or no)"<<endl;
-               cin>>response3;
-               if (response3=="y")
                   continue;
                else
                   cout<<"Thanks for playing!"<<endl;
                   break;
+            }
+            else
+                cout<<"You establishes a point, you need to get the sum of: "<<sum<<". Roll again? (y or no)"<<endl;
+                cin>>response3;
+
+               
+                int sum2,dice_output3,dice_output4;
+                if (response3=="y")
+                {
+                   continue;
+                    /*
+                    Cannot make the point to work properly
+                    play2 = true;
+                    while (play2)
+                        dice_output3 = rand()%6+1; //change this expression so dice_output has a value between 1 and 6
+                        dice_output4 = rand()%6+1  ; 
+                        sum2 = dice_output3 + dice_output4;
+                        cout <<"You rolled: "<<dice_output3 <<" and "<<dice_output4<<endl;
+                        cout <<"So your sum is: "<<sum2<<endl;
+                        if (sum2 == sum)
+                    
+                            cout<<"You win!"<<endl;
+                            cout<<"Continue?(y or no)"<<endl;
+                       
+                            if (response4=="y")
+                               continue;
+                            else
+                               break;
+                        if (sum2 == 7)
+                            cout<<"You lose"<<endl;
+                            cout<<"Continue?(y or no)"<<endl;
+                       
+                            if (response5=="y")
+                               continue;
+                            else
+                               break;
+                        */       
+
+                }
+                    //else
+                       //continue;
+                else
+                {
+
+                  cout<<"Thanks for playing!"<<endl;
+                  break;
+                }
             }
 
         }
